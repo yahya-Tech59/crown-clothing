5bf66c7fd55b87342a89aae7b3867697e2d583c8
yahya-cade <yahyakade790@gmail.com>
2023-11-27 10:54:54 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/Navigation";
+// import SignIn from "./routes/sign-in/sign-in";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        {/* <Route path="sign-in" element={<SignIn />} /> */}
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
